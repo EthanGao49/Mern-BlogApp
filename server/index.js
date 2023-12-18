498d58e3b40d96245b3ed84c32e719fce7610c80
EthanGao49 <ethan.gao47@gmail.com>
2023-12-17 23:38:43 -0500

    support user auth in server

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
 
 /* CONFIGURATIONS */
@@ -38,6 +39,9 @@ const upload = multer({ storage });
 /* ROUTE WITH FILES */
 // Call the api in auth/register
 app.post("/auth/register", upload.single("picture"), register);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
